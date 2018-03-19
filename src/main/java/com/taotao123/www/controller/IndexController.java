017d3cfa45f2a0a91fd719588fa5fd4f4cc7888c
liuzhengtao <liuzt@yuminsoft.com>
2018-03-19 13:35:11 +0800

    delp提交代码

---
+++ src/main/java/com/taotao123/www/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.taotao123.www.controller;
+
+/**
+ * <pre>
+ *     author : user
+ *     e-mail : liuzt@yuminsoft.com
+ *     time   : 2018/03/19
+ *     version: 1.0
+ * </pre>
+ */
+public class IndexController {
+
+}
